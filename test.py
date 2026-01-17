29d89d2edc4a2708d9915574c8c3e5203d179622
BuildTools <unconfigured@null.spigotmc.org>
2026-01-17 14:57:21 +0100

    jen testování

--- test.py
+++ test.py
@@ -6,4 +6,14 @@ import time
 import difflib
 import string
 
-print("abCUIHIUGuhiuh".capitalize())
+def func1():
+    def func2():
+        def func3():
+            def func4():
+                print(a)
+            func4()
+        func3()
+    func2()
+
+a = 1
+func1()
